4f0718f545d22168f841991d68607f2e552a0571
Siim Härma <siimharma267@gmail.com>
2023-09-10 11:07:56 +0300

    week1 code

---
+++ server.js
@@ -0,0 +1,12 @@
+
+let firstName = "Harry";
+let lastName = "Potter";
+
+//console.log("Hello, " + firstName + "!");
+
+//template literals
+
+//console.log(`Hello, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
